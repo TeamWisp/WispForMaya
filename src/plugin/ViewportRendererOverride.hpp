3aad059e18019c6a53b67b3f0dcafbe18c0171cc
Tahar Meijs <tntmeijs@gmail.com>
2018-11-23 12:01:05 +0100

    Viewport renderer override virtual functions
    [ADD] Start operation override.
    [ADD] Render operation override.
    [ADD] Next render operation override.
    [ADD] UI name override.
    [ADD] Setup override.
    [ADD] Cleanup override.

--- src/plugin/ViewportRendererOverride.hpp
+++ src/plugin/ViewportRendererOverride.hpp
@@ -10,14 +10,27 @@ namespace wisp
 	{
 	public:
 		ViewportRendererOverride(const MString& t_name);
-		~ViewportRendererOverride();
+		~ViewportRendererOverride() final override;
 
 		// This plug-in supports all rendering APIs (only has to show the output of the Wisp renderer)
 		MHWRender::DrawAPI supportedDrawAPIs() const final override;
-		
+
+		bool startOperationIterator() final override;
+
+		MHWRender::MRenderOperation* renderOperation() final override;
+
+		bool nextRenderOperation() final override;
+
+		MString uiName() const final override;
+
+		MStatus setup(const MString& t_destination) final override;
+		MStatus cleanup() final override;
+
 	private:
 		// This is the name that will appear in the "Renderer" menu drop-down
 		MString m_plugin_name;
+
+		int m_render_operation_iterator;
 	};
 }
 
