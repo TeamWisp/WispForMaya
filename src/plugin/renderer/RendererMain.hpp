654b531dba2f94af457168291fb5e8f7b0a46c15
Tahar Meijs <tntmeijs@gmail.com>
2018-11-27 14:48:32 +0100

    Wisp renderer demo
    [ADD] Wisp renderer demo source code.

--- src/plugin/renderer/RendererMain.hpp
+++ src/plugin/renderer/RendererMain.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "imgui_tools.hpp"
+
+#include <memory>
+
 namespace wmr::wri
 {
 	class RendererMain
@@ -8,6 +12,23 @@ namespace wmr::wri
 		RendererMain();
 		~RendererMain();
 
+		void StartWispRenderer();
+
 	private:
+		void RenderEditor();
+
+	private:
+		std::unique_ptr<wr::D3D12RenderSystem> m_render_system;
+		static wr::imgui::special::DebugConsole m_debug_console;
+
+		// TODO: Remove these ImGui helper variables once a real ImGui
+		// tool is in place, this is just to demonstrate ImGui running
+		bool m_main_menu;
+		bool m_open0;
+		bool m_open1;
+		bool m_open2;
+		bool m_open_console;
+		bool m_show_imgui;
+		char message_buffer[600];
 	};
 }
