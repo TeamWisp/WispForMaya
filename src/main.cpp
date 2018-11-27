804339cc385023368355ba0cff4f5b6a4ceb71f0
Tahar Meijs <tntmeijs@gmail.com>
2018-11-27 09:55:39 +0100

    Main file restructure
    [ADD] Utility functions to make the code more modular.
    [CHANGE] Global instance raw pointer into unique pointer.

--- src/main.cpp
+++ src/main.cpp
@@ -10,68 +10,101 @@
 #include "Settings.hpp"
 #include "plugin/ViewportRendererOverride.hpp"
 
+namespace wmr::util
+{
+	void ThrowIfFailed(const MStatus& t_status);
+	void RegisterPlugin(MHWRender::MRenderer* const t_maya_renderer);
+	void UnregisterPlugin(MHWRender::MRenderer* const t_maya_renderer);
+
+	bool IsSceneDirty();
+}
+
 MStatus initializePlugin(MObject obj)
 {
-	MStatus status;
 	MFnPlugin plugin(obj, wisp::settings::COMPANY_NAME, wisp::settings::PRODUCT_VERSION, "Any");
 
 	// Workaround for avoiding dirtying the scene until there is a way to
 	// register overrides without causing dirty.
-	bool sceneDirty = true;
+	bool is_scene_dirty = true;
+	is_scene_dirty = wmr::util::IsSceneDirty();
 
-	try
-	{
-		// Is the scene currently dirty?
-		MCommandResult sceneDirtyResult(&status);
-		if (status != MStatus::kSuccess) throw std::exception();
-		status = MGlobal::executeCommand("file -query -modified", sceneDirtyResult);
-		if (status != MStatus::kSuccess) throw std::exception();
-		int commandResult;
-		status = sceneDirtyResult.getResult(commandResult);
-		if (status != MStatus::kSuccess) throw std::exception();
-		sceneDirty = commandResult != 0;
-	}
-	catch (std::exception&)
-	{
-		sceneDirty = true;
-	}
-
-	MHWRender::MRenderer* renderer = MHWRender::MRenderer::theRenderer();
-	if (renderer)
-	{
-		if (!wmr::WispViewportRenderer::sViewImageBlitOverrideInstance)
-		{
-			wmr::WispViewportRenderer::sViewImageBlitOverrideInstance = new wmr::WispViewportRenderer("my_viewImageBlitOverride");
-			renderer->registerOverride(wmr::WispViewportRenderer::sViewImageBlitOverrideInstance);
-		}
-	}
+	wmr::util::RegisterPlugin(MHWRender::MRenderer::theRenderer());
 
 	// If the scene was previously unmodified, return it to that state since
 	// there are no changes that need to be saved
-	if (sceneDirty == false)
+	if (is_scene_dirty == false)
 	{
 		MGlobal::executeCommand("file -modified 0");
 	}
 
-	return status;
+	return MStatus::kSuccess;
 }
 
-MStatus uninitializePlugin(MObject obj)
+bool wmr::util::IsSceneDirty()
 {
-	MStatus status;
-	MFnPlugin plugin(obj);
+	MStatus status = MStatus::kFailure;
 
-	MHWRender::MRenderer* renderer = MHWRender::MRenderer::theRenderer();
-	if (renderer)
+	try
 	{
-		if (wmr::WispViewportRenderer::sViewImageBlitOverrideInstance)
-		{
-			renderer->deregisterOverride(wmr::WispViewportRenderer::sViewImageBlitOverrideInstance);
-			delete wmr::WispViewportRenderer::sViewImageBlitOverrideInstance;
-		}
-		wmr::WispViewportRenderer::sViewImageBlitOverrideInstance = nullptr;
+		// Is the scene currently dirty?
+		MCommandResult scene_dirty_result(&status);
+		wmr::util::ThrowIfFailed(status);
+
+		status = MGlobal::executeCommand("file -query -modified", scene_dirty_result);
+		wmr::util::ThrowIfFailed(status);
+
+		int command_result = -1;
+		status = scene_dirty_result.getResult(command_result);
+		wmr::util::ThrowIfFailed(status);
+
+		return (command_result != 0);
+	}
+	catch (std::exception&)
+	{
+		return true;
+	}
+}
+
+void wmr::util::ThrowIfFailed(const MStatus& t_status)
+{
+	if (t_status != MStatus::kSuccess)
+	{
+		throw std::exception();
+	}
+}
+
+void wmr::util::RegisterPlugin(MHWRender::MRenderer* const t_maya_renderer)
+{
+	if (!t_maya_renderer)
+	{
+		return;
 	}
 
-	return status;
+	if (!wmr::WispViewportRenderer::global_viewport_renderer_instance)
+	{
+		wmr::WispViewportRenderer::global_viewport_renderer_instance = std::make_unique<wmr::WispViewportRenderer>("my_viewImageBlitOverride");
+		t_maya_renderer->registerOverride(wmr::WispViewportRenderer::global_viewport_renderer_instance.get());
+	}
 }
 
+MStatus uninitializePlugin(MObject t_object)
+{
+	MFnPlugin plugin(t_object);
+
+	wmr::util::UnregisterPlugin(MHWRender::MRenderer::theRenderer());
+
+	return MStatus::kSuccess;
+}
+
+void wmr::util::UnregisterPlugin(MHWRender::MRenderer* const t_maya_renderer)
+{
+	if (!t_maya_renderer)
+	{
+		return;
+	}
+
+	if (wmr::WispViewportRenderer::global_viewport_renderer_instance)
+	{
+		t_maya_renderer->deregisterOverride(wmr::WispViewportRenderer::global_viewport_renderer_instance.get());
+	}
+}
