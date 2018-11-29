d117dce2447cc2434b60dfd8c40f072b24dac48b
Tahar Meijs <tntmeijs@gmail.com>
2018-11-29 14:20:38 +0100

    Wisp renderer proper initialization
    [CHANGE] Nullptr argument into a std::nullopt.
    [CHANGE] Shader locations.
    [CHANGE] Files to be hard-coded for now. This must be removed in the future.

--- src/plugin/overrides/ViewportRendererOverride.cpp
+++ src/plugin/overrides/ViewportRendererOverride.cpp
@@ -19,15 +19,15 @@ wmr::WispViewportRenderer::WispViewportRenderer(const MString& t_name)
 	m_color_texture.texture = nullptr;
 	m_color_texture_desc.setToDefault2DTexture();
 
-	// "Entry point" for the Wisp renderer
-	m_wisp_renderer = std::make_unique<wri::RendererMain>();
-	m_wisp_renderer->StartWispRenderer();
+	// Entry point for the Wisp renderer
+	///m_wisp_renderer = std::make_unique<wri::RendererMain>();
+	///m_wisp_renderer->StartWispRenderer();
 }
 
 wmr::WispViewportRenderer::~WispViewportRenderer()
 {
 	// This will clean-up any Wisp resources
-	m_wisp_renderer->StopWispRenderer();
+	///m_wisp_renderer->StopWispRenderer();
 
 	MHWRender::MRenderer* maya_renderer = MHWRender::MRenderer::theRenderer();
 	MHWRender::MTextureManager* maya_texture_manager = maya_renderer ? maya_renderer->getTextureManager() : nullptr;
