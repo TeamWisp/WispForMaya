7d78a96755036a9f7b66bc5cc229bbe4338b0980
Tahar Meijs <tntmeijs@gmail.com>
2018-11-30 09:49:20 +0100

    No more debug strings
    [DELETE] Console debug output.

--- src/plugin/renderer/RendererMain.cpp
+++ src/plugin/renderer/RendererMain.cpp
@@ -1,9 +1,5 @@
 #include "RendererMain.hpp"
 
-// TODO: Remove this once debugging is over
-#include <maya/MGlobal.h>
-#include <maya/MStreamUtils.h>
-
 wmr::wri::RendererMain::RendererMain()
 {
 }
@@ -18,7 +14,6 @@ void wmr::wri::RendererMain::StartWispRenderer()
 
 void wmr::wri::RendererMain::UpdateWispRenderer()
 {
-	MStreamUtils::stdOutStream() << "Wisp renderer update!" << "\n";
 }
 
 void wmr::wri::RendererMain::StopWispRenderer()
