0b66cee9333875f794386a83ee5a45829afa4823
Tahar Meijs <tntmeijs@gmail.com>
2018-12-03 13:00:46 +0100

    Initialization of the Wisp renderer
    [ADD] Wisp rendering framework initialization.

--- src/plugin/renderer/RendererMain.hpp
+++ src/plugin/renderer/RendererMain.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
+#include "frame_graph/frame_graph.hpp"
+
+#include <memory>
+
+namespace wr
+{
+	class D3D12RenderSystem;
+	class SceneGraph;
+}
+
 namespace wmr::wri
 {
 	class RendererMain
@@ -10,8 +20,12 @@ namespace wmr::wri
 
 		void Initialize();
 		void Update();
+		void Resize(unsigned int t_new_width, unsigned int t_new_height);
 		void Cleanup();
 
 	private:
+		std::unique_ptr<wr::D3D12RenderSystem> m_render_system;
+		std::unique_ptr<wr::FrameGraph> m_frame_graph;
+		std::shared_ptr<wr::SceneGraph> m_scene_graph;
 	};
 }
