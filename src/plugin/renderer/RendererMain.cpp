045045c89c2f282b3f871d021f507bf60f88af02
Tahar Meijs <tntmeijs@gmail.com>
2018-12-03 13:00:46 +0100

    Initialization of the Wisp renderer
    [ADD] Wisp rendering framework initialization.

--- src/plugin/renderer/RendererMain.cpp
+++ src/plugin/renderer/RendererMain.cpp
@@ -1,10 +1,22 @@
 #include "RendererMain.hpp"
 
+#include "wisp.hpp"
+#include "render_tasks/d3d12_test_render_task.hpp"
+
 void wmr::wri::RendererMain::Initialize()
 {
+	m_render_system = std::make_unique<wr::D3D12RenderSystem>();
+	m_frame_graph = std::make_unique<wr::FrameGraph>();
+
+	m_render_system->Init(std::nullopt);
 }
 
 void wmr::wri::RendererMain::Update()
+{
+	// TODO: Update the framework logic in here!
+}
+
+void wmr::wri::RendererMain::Resize(unsigned int t_new_width, unsigned int t_new_height)
 {
 }
 
