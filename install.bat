fb7b041f1ecdb36ed5f09fa4a93a64187b1ee1d8
Jens Hagen <hagen-j@outlook.com>
2018-12-11 10:52:22 +0100

    [change] removed submodule update for Procedural-ray-tracing project in the install.bat

--- install.bat
+++ install.bat
@@ -90,12 +90,7 @@ REM ##### DOWNLOAD DEPS #####
 call :colorEcho %header_color% "#### Downloading Dependencies ####"
 cd "%workspace_path%"
 git submodule init
-git submodule update  --recursive --remote
-:: init and update Procedural ray tracing project deps.
-cd deps\Procedural-Ray-Tracing
-git submodule init
-git submodule update  --recursive --remote
-cd ../../
+git submodule update --recursive --remote
 EXIT /B 0
 REM ##### DOWNLOAD DEPS #####
 
