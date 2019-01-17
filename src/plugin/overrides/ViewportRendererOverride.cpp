383b2e8456a389681e57a8d77b204f78e518bde4
Tahar Meijs <tntmeijs@gmail.com>
2019-01-17 16:40:37 +0100

    Wisp depth buffer copy
    [ADD] Depth buffer data from Wisp.

--- src/plugin/overrides/ViewportRendererOverride.cpp
+++ src/plugin/overrides/ViewportRendererOverride.cpp
@@ -41,7 +41,6 @@
 #include <maya/MGlobal.h>
 
 auto window = std::make_unique<wr::Window>( GetModuleHandleA( nullptr ), "D3D12 Test App", 1280, 720 );
-const bool load_images_from_disk = true;
 
 #define SCENE viknell_scene
 
@@ -67,12 +66,12 @@ namespace wmr
 		, m_current_render_operation(-1)
 	{
 		ConfigureRenderOperations();
-		SetDefaultColorTextureState();
+		SetDefaultTextureState();
 	}
 
 	ViewportRenderer::~ViewportRenderer()
 	{
-		ReleaseColorTextureResources();
+		ReleaseTextureResources();
 	}
 
 	void ViewportRenderer::Initialize()
@@ -88,7 +87,6 @@ namespace wmr
 		m_model_loader.reset();
 		m_render_system.reset();
 		m_framegraph.reset();
-
 	}
 
 	void ViewportRenderer::ConfigureRenderOperations()
@@ -98,29 +96,32 @@ namespace wmr
 		m_render_operation_names[2] = "wisp_Present";
 	}
 
-	void ViewportRenderer::SetDefaultColorTextureState()
+	void ViewportRenderer::SetDefaultTextureState()
 	{
 		m_color_texture.texture = nullptr;
 		m_color_texture_desc.setToDefault2DTexture();
+
+		m_depth_texture.texture = nullptr;
+		m_depth_texture_desc.setToDefault2DTexture();
 	}
 
-	void ViewportRenderer::ReleaseColorTextureResources() const
+	void ViewportRenderer::ReleaseTextureResources() const
 	{
 		const auto maya_renderer = MHWRender::MRenderer::theRenderer();
 
 		if (!maya_renderer)
-		{
 			return;
-		}
 
 		const auto maya_texture_manager = maya_renderer->getTextureManager();
 
-		if (!maya_texture_manager || !m_color_texture.texture)
-		{
+		if (!maya_texture_manager)
 			return;
-		}
 
-		maya_texture_manager->releaseTexture(m_color_texture.texture);
+		if (m_color_texture.texture)
+			maya_texture_manager->releaseTexture(m_color_texture.texture);
+
+		if (m_depth_texture.texture)
+			maya_texture_manager->releaseTexture(m_depth_texture.texture);
 	}
 
 	void ViewportRenderer::CreateRenderOperations()
@@ -301,6 +302,75 @@ namespace wmr
 				!m_render_operations[3]);
 	}
 
+	// TODO: REFACTOR
+	void ViewportRenderer::UpdateTextureData(MHWRender::MTextureAssignment& texture_to_update, WispBufferType type, const wr::CPUTexture& cpu_texture, MHWRender::MTextureManager* texture_manager)
+	{
+		unsigned int buffer_width = cpu_texture.m_buffer_width;
+		unsigned int buffer_height = cpu_texture.m_buffer_height;
+		unsigned int buffer_bytes_per_pixel = cpu_texture.m_bytes_per_pixel;
+
+		// Allocate memory to store the Wisp texture data
+		auto* wisp_data = new unsigned char[buffer_width * buffer_height * buffer_bytes_per_pixel];
+		memcpy(wisp_data, cpu_texture.m_data, sizeof(unsigned char) * buffer_width * buffer_height * buffer_bytes_per_pixel);
+
+		if (!texture_to_update.texture)
+		{
+			switch (type)
+			{
+			case WispBufferType::COLOR:
+				m_color_texture_desc.fWidth = buffer_width;
+				m_color_texture_desc.fHeight = buffer_height;
+				m_color_texture_desc.fDepth = 1;
+				m_color_texture_desc.fBytesPerRow = buffer_bytes_per_pixel * buffer_width;
+				m_color_texture_desc.fBytesPerSlice = m_color_texture_desc.fBytesPerRow * buffer_height;
+				m_color_texture_desc.fTextureType = MHWRender::kImage2D;
+
+				// Acquire a new texture
+				m_color_texture.texture = texture_manager->acquireTexture("", m_color_texture_desc, wisp_data);
+
+				if (m_color_texture.texture)
+					m_color_texture.texture->textureDescription(m_color_texture_desc);
+				break;
+
+			case WispBufferType::DEPTH:
+				m_depth_texture_desc.fWidth = buffer_width;
+				m_depth_texture_desc.fHeight = buffer_height;
+				m_depth_texture_desc.fDepth = 1;
+				m_depth_texture_desc.fBytesPerRow = buffer_bytes_per_pixel * buffer_width;
+				m_depth_texture_desc.fBytesPerSlice = m_color_texture_desc.fBytesPerRow * buffer_height;
+				m_depth_texture_desc.fTextureType = MHWRender::kDepthTexture;
+
+				// Acquire a new texture
+				m_depth_texture.texture = texture_manager->acquireDepthTexture("", reinterpret_cast<float*>(wisp_data), buffer_width, buffer_height, false, nullptr);
+
+				if (m_depth_texture.texture)
+					m_depth_texture.texture->textureDescription(m_depth_texture_desc);
+				break;
+
+			default:
+				break;
+			}
+		}
+		else
+		{
+			switch (type)
+			{
+			case wmr::WispBufferType::COLOR:
+				m_color_texture.texture->update(wisp_data, false);
+				break;
+			
+			case wmr::WispBufferType::DEPTH:
+				m_depth_texture.texture->update(wisp_data, false);
+				break;
+
+			default:
+				break;
+			}
+		}
+
+		delete[] wisp_data;
+	}
+
 	MStatus ViewportRenderer::cleanup()
 	{
 		m_current_render_operation = -1;
@@ -330,34 +400,6 @@ namespace wmr
 		return false;
 	}
 
-	static void loadImageFromDisk( MString& image_location, MHWRender::MTextureDescription& color_texture_desc, MHWRender::MTextureAssignment& color_texture, MHWRender::MTextureManager* texture_manager )
-	{
-		unsigned char* texture_data = nullptr;
-
-		MImage image;
-
-		unsigned int target_width, target_height;
-
-		image.readFromFile( image_location );
-		image.getSize( target_width, target_height );
-
-		texture_data = image.pixels();
-
-		color_texture_desc.fWidth = target_width;
-		color_texture_desc.fHeight = target_height;
-		color_texture_desc.fDepth = 1;
-		color_texture_desc.fBytesPerRow = 4 * target_width;
-		color_texture_desc.fBytesPerSlice = color_texture_desc.fBytesPerRow * target_height;
-
-		// Acquire a new texture
-		color_texture.texture = texture_manager->acquireTexture( "", color_texture_desc, texture_data );
-
-		if( color_texture.texture )
-		{
-			color_texture.texture->textureDescription( color_texture_desc );
-		}
-	}
-
 	bool ViewportRenderer::UpdateTextures(MHWRender::MRenderer* maya_renderer, MHWRender::MTextureManager* texture_manager, const wr::CPUTextures& cpu_textures)
 	{
 		if (!maya_renderer || !texture_manager)
@@ -381,58 +423,36 @@ namespace wmr
 		// Release any existing textures.
 		//
 		if (force_reload || !m_color_texture.texture ||
-			(m_color_texture_desc.fWidth != target_width || m_color_texture_desc.fHeight != target_height))
+			(m_color_texture_desc.fWidth != target_width || m_color_texture_desc.fHeight != target_height) ||
+			(m_depth_texture_desc.fWidth != target_width || m_depth_texture_desc.fHeight != target_height))
 		{
 			if (m_color_texture.texture)
 			{
 				texture_manager->releaseTexture(m_color_texture.texture);
-				m_color_texture.texture = NULL;
+				m_color_texture.texture = nullptr;
+			}
+
+			if (m_depth_texture.texture)
+			{
+				texture_manager->releaseTexture(m_depth_texture.texture);
+				m_depth_texture.texture = nullptr;
 			}
 			
 			aquire_new_texture = true;
 			force_reload = false;
 		}
 
-		if (!m_color_texture.texture)
-		{
-			// Grab the Wisp texture data
-			auto* texture_data = new unsigned char[cpu_textures.pixel_data.value().m_buffer_width * cpu_textures.pixel_data.value().m_buffer_height * cpu_textures.pixel_data.value().m_bytes_per_pixel];
-			memcpy(texture_data, cpu_textures.pixel_data.value().m_data, cpu_textures.pixel_data.value().m_buffer_width * cpu_textures.pixel_data.value().m_buffer_height * cpu_textures.pixel_data.value().m_bytes_per_pixel);
-
-			m_color_texture_desc.fWidth = cpu_textures.pixel_data.value().m_buffer_width;
-			m_color_texture_desc.fHeight = cpu_textures.pixel_data.value().m_buffer_height;
-			m_color_texture_desc.fDepth = 1;
-			m_color_texture_desc.fBytesPerRow = cpu_textures.pixel_data.value().m_bytes_per_pixel * cpu_textures.pixel_data.value().m_buffer_width;
-			m_color_texture_desc.fBytesPerSlice = m_color_texture_desc.fBytesPerRow * cpu_textures.pixel_data.value().m_buffer_height;
-
-			// Acquire a new texture.
-			m_color_texture.texture = texture_manager->acquireTexture("", m_color_texture_desc, texture_data);
-
-			if (m_color_texture.texture)
-				m_color_texture.texture->textureDescription(m_color_texture_desc);
-
-			// Don't need the data any more after upload to texture
-			delete[] texture_data;
-		}
-		else
-		{
-			unsigned char* texture_data = nullptr;
-
-			// Grab the Wisp texture data
-			texture_data = new unsigned char[cpu_textures.pixel_data.value().m_buffer_width * cpu_textures.pixel_data.value().m_buffer_height * cpu_textures.pixel_data.value().m_bytes_per_pixel];
-
-			m_color_texture.texture->update(texture_data, false);
-			delete[] texture_data;
-		}
+		UpdateTextureData(m_color_texture, WispBufferType::COLOR, cpu_textures.pixel_data.value(), texture_manager);
+		UpdateTextureData(m_depth_texture, WispBufferType::DEPTH, cpu_textures.depth_data.value(), texture_manager);
 
 		// Update the textures used for the blit operation.
-		//
 		if (aquire_new_texture)
 		{
 			auto* custom_blit = (WispScreenBlitter*)m_render_operations[0].get();
 			custom_blit->SetColorTexture(m_color_texture);
+			custom_blit->SetDepthTexture(m_depth_texture);
 		}
 
-		return m_color_texture.texture ? true : false;
+		return (m_color_texture.texture && m_depth_texture.texture);
 	}
 }
