dedfea12cd80fbfdc3811a516b451ceaf54fff6b
Tahar Meijs <tntmeijs@gmail.com>
2019-02-11 16:05:09 +0100

    Callback manager documentation
    [ADD] "CallbackManager.hpp" documentation.
    [ADD] "CallbackManager.cpp" documentation.

--- src/plugin/CallbackManager.cpp
+++ src/plugin/CallbackManager.cpp
@@ -1,32 +1,35 @@
 #include "CallbackManager.hpp"
-#include <maya\MCallbackIdArray.h>
+
+// Maya API
+#include <maya/MCallbackIdArray.h>
 
 namespace wmr
 {
 	CallbackManager* CallbackManager::m_instance = nullptr;
 
-	CallbackManager::CallbackManager()
-	{
-	}
-
 	CallbackManager::~CallbackManager()
 	{
+		// Calling this just in case the user did not...
 		Reset();
 	}
 
 	CallbackManager& CallbackManager::GetInstance()
 	{
-		return m_instance == nullptr ? *( m_instance = new CallbackManager() ) : *m_instance;
+		// Return the instance of the class, or nullptr if it has not been set yet
+		return m_instance == nullptr ? *(m_instance = new CallbackManager()) : *m_instance;
 	}
 
-	void CallbackManager::RegisterCallback( MCallbackId mcid )
+	void CallbackManager::RegisterCallback(MCallbackId mcid)
 	{
-		m_callback_vector.push_back( mcid );
+		// Save the callback ID for future use
+		m_callback_vector.push_back(mcid);
 	}
 
 	void CallbackManager::Reset()
 	{
 		size_t count = m_callback_vector.size();
+
+		// Only reset the callback if there are any in the first place
 		if( count > 0 )
 		{
 			MCallbackIdArray( m_callback_vector.data(), count );
