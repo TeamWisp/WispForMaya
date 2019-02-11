39067d14852d7eaa2bbae38a64ae68713e9552d0
Tahar Meijs <tntmeijs@gmail.com>
2019-02-11 15:46:21 +0100

    Namespace documentation
    [ADD] Each namespace in the file has been documented.

--- src/miscellaneous/Functions.hpp
+++ src/miscellaneous/Functions.hpp
@@ -3,20 +3,25 @@
 // Maya API
 #include <maya/MStatus.h>
 
-namespace wmr::func
+//! Generic plug-in namespace (Wisp Maya Renderer)
+namespace wmr
 {
-	//! Throws an exception upon failure
-	/*! This function should only be used on critical systems, as it will trigger a Maya crash. Please do not use this as
-	 *! a standard "generic" check.
-	 *!
-	 *! \param status The status to be evaluated.
-	 *! \return No return value, but an exception will be thrown when status == MStatus::kSuccess. */
-	void ThrowIfFailedMaya(const MStatus& status);
+	//! Holds stand-alone utility functions
+	namespace func
+	{
+		//! Throws an exception upon failure
+		/*! This function should only be used on critical systems, as it will trigger a Maya crash. Please do not use this as
+		 *! a standard "generic" check.
+		 *!
+		 *! \param status The status to be evaluated.
+		 *! \return No return value, but an exception will be thrown when status == MStatus::kSuccess. */
+		void ThrowIfFailedMaya(const MStatus& status);
 
-	//! Output a debug message to the console
-	/*! Utility function that logs to the Visual Studio console window on Windows. On other systems, "printf" is used instead.
-	 *! If desired, a platform-specific implementation may be created here.
-	 *!
-	 *! \param msg The message to log to the output. */
-	void LogDebug(const char* msg);
+		//! Output a debug message to the console
+		/*! Utility function that logs to the Visual Studio console window on Windows. On other systems, "printf" is used instead.
+		 *! If desired, a platform-specific implementation may be created here.
+		 *!
+		 *! \param msg The message to log to the output. */
+		void LogDebug(const char* msg);
+	}
 }
