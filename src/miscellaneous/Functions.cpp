cbadeb71567f6a71413074649b5a5c4ab580b531
Tahar Meijs <tntmeijs@gmail.com>
2019-02-11 15:42:06 +0100

    Documentation in header
    [CHANGE] Documentation has been moved to the header file.

--- src/miscellaneous/Functions.cpp
+++ src/miscellaneous/Functions.cpp
@@ -1,15 +1,10 @@
 #include "Functions.hpp"
 
+// Windows
 #include <Windows.h>
 
 namespace wmr::func
 {
-	//! Throws an exception upon failure
-	/*! This function should only be used on critical systems, as it will trigger a Maya crash. Please do not use this as
-	 *! a standard "generic" check.
-	 *! 
-	 *! \param status The status to be evaluated.
-	 *! \return No return value, but an exception will be thrown when status == MStatus::kSuccess. */
 	void ThrowIfFailedMaya(const MStatus& status)
 	{
 		if (status != MStatus::kSuccess)
@@ -18,14 +13,10 @@ namespace wmr::func
 		}
 	}
 
-	//! Output a debug message to the console
-	/*! Utility function that logs to the Visual Studio console window on Windows. On other systems, "printf" is used instead.
-	 *! If desired, a platform-specific implementation may be created here.
-	 *! 
-	 *! \param msg The message to log to the output. */
 	void LogDebug(const char* msg)
 	{
 #if defined(WIN32) && defined(_DEBUG)
+		// Log to the Visual Studio debug output
 		OutputDebugStringA(msg);
 #else
 		printf("%s", msg);
--- src/miscellaneous/Functions.hpp
+++ src/miscellaneous/Functions.hpp
@@ -1,12 +1,22 @@
 #pragma once
 
+// Maya API
 #include <maya/MStatus.h>
 
 namespace wmr::func
 {
-	// Throws exception when Status::kSuccess is false
+	//! Throws an exception upon failure
+	/*! This function should only be used on critical systems, as it will trigger a Maya crash. Please do not use this as
+	 *! a standard "generic" check.
+	 *!
+	 *! \param status The status to be evaluated.
+	 *! \return No return value, but an exception will be thrown when status == MStatus::kSuccess. */
 	void ThrowIfFailedMaya(const MStatus& status);
 
-	// Wraps the Visual Studio debug output function nicely
+	//! Output a debug message to the console
+	/*! Utility function that logs to the Visual Studio console window on Windows. On other systems, "printf" is used instead.
+	 *! If desired, a platform-specific implementation may be created here.
+	 *!
+	 *! \param msg The message to log to the output. */
 	void LogDebug(const char* msg);
 }
