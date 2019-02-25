eca9e3dd0212484d6d4435cc962155b961fe798f
Tahar Meijs <tntmeijs@gmail.com>
2019-02-25 10:55:04 +0100

    Texture manager default texture
    [ADD] Default texture.
    [ADD] Default texture retrieval function.

--- src/plugin/renderer/texture_manager.cpp
+++ src/plugin/renderer/texture_manager.cpp
@@ -1,9 +1,19 @@
 #include "texture_manager.hpp"
 
-wmr::TextureManager::TextureManager()
-{
-}
+// Wisp rendering framework
+#include "wisp.hpp"
 
-wmr::TextureManager::~TextureManager()
+namespace wmr
 {
+	TextureManager::TextureManager()
+	{
+		// The default texture needs to be loaded at all times
+		m_texture_container.push_back(m_texture_pool->Load("./resources/textures/default_texture.png", false, false));
+	}
+
+	const wr::TextureHandle& TextureManager::GetDefaultTexture() const noexcept
+	{
+		// This is safe because this class ensures that there is always a fall-back texture on index 0
+		return m_texture_container[0];
+	}
 }
