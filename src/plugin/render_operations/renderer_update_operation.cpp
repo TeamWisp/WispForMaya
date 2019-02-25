7df49c871d1df78378c72fb177198504d4de462f
Tahar Meijs <tntmeijs@gmail.com>
2019-02-25 14:05:59 +0100

    Wisp update operation
    [ADD] Wisp renderer reference.
    [ADD] Wisp update call.

--- src/plugin/render_operations/renderer_update_operation.cpp
+++ src/plugin/render_operations/renderer_update_operation.cpp
@@ -1,9 +1,18 @@
 #include "renderer_update_operation.hpp"
 
+// Wisp plug-in
+#include "miscellaneous/settings.hpp"
+#include "plugin/renderer/renderer.hpp"
+#include "plugin/viewport_renderer_override.hpp"
+
+// Maya API
+#include <maya/MViewport2Renderer.h>
+
 namespace wmr
 {
 	RendererUpdateOperation::RendererUpdateOperation(const MString & name)
 		: MHWRender::MUserRenderOperation(name)
+		, m_renderer(dynamic_cast<const ViewportRendererOverride*>(MHWRender::MRenderer::theRenderer()->findRenderOverride(settings::VIEWPORT_OVERRIDE_NAME))->GetRenderer())
 	{
 	}
 
@@ -19,12 +28,15 @@ namespace wmr
 
 	MStatus RendererUpdateOperation::execute(const MDrawContext& draw_context)
 	{
+		// Update the Wisp rendering framework to prepare it for rendering
+		m_renderer.Update();
+
 		return MStatus::kSuccess;
 	}
 
 	bool RendererUpdateOperation::hasUIDrawables() const
 	{
-		// Not using any UI drawables
+		// Not using any UI drawable
 		return false;
 	}
 
