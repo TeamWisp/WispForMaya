e778c67957e1b41bf8dff8c303da212c65c0d9fc
Tahar Meijs <tntmeijs@gmail.com>
2019-02-26 16:37:07 +0100

    Camera viewport panel updates
    [ADD] Once the callback is hooked up, the viewport camera will synchronize using the current active viewport. If all goes well, this code should be capable of rendering all possible Maya viewport configurations.

--- src/plugin/parsers/camera_parser.cpp
+++ src/plugin/parsers/camera_parser.cpp
@@ -1,9 +1,56 @@
 #include "camera_parser.hpp"
 
-wmr::CameraParser::CameraParser()
+// Wisp plug-in
+#include "plugin/renderer/renderer.hpp"
+#include "plugin/viewport_renderer_override.hpp"
+
+// Wisp rendering framework
+#include "scene_graph/camera_node.hpp"
+
+// Maya API
+#include <maya/M3dView.h>
+#include <maya/MEulerRotation.h>
+#include <maya/MFnCamera.h>
+#include <maya/MFnTransform.h>
+#include <maya/MViewport2Renderer.h>
+
+void wmr::CameraParser::Initialize()
 {
+	m_viewport_camera = dynamic_cast<const ViewportRendererOverride*>(MHWRender::MRenderer::theRenderer()->findRenderOverride(settings::VIEWPORT_OVERRIDE_NAME))->GetRenderer().GetCamera();
 }
 
-wmr::CameraParser::~CameraParser()
+void wmr::CameraParser::UpdateViewportCamera(const MString & panel_name)
 {
+	M3dView viewport;
+
+	// Try to retrieve the current active viewport panel
+	auto status = M3dView::getM3dViewFromModelPanel(panel_name, viewport);
+
+	// Could not retrieve the viewport panel
+	if (!status)
+		return;
+
+	// Model view matrix
+	MMatrix model_view_matrix;
+	viewport.modelViewMatrix(model_view_matrix);
+
+	MDagPath camera_dag_path;
+	viewport.getCamera(camera_dag_path);
+	MFnTransform camera_transform(camera_dag_path.transform());
+
+	MEulerRotation view_rotation;
+	camera_transform.getRotation(view_rotation);
+
+	m_viewport_camera->SetRotation({ (float)view_rotation.x,(float)view_rotation.y, (float)view_rotation.z });
+
+	MMatrix cameraPos = camera_dag_path.inclusiveMatrix();
+	MVector eye = MVector(static_cast<float>(cameraPos(3, 0)), static_cast<float>(cameraPos(3, 1)), static_cast<float>(cameraPos(3, 2)));
+	m_viewport_camera->SetPosition({ (float)-eye.x, (float)-eye.y, (float)-eye.z });
+
+	MFnCamera camera_functions(camera_dag_path);
+	m_viewport_camera->m_frustum_far = camera_functions.farClippingPlane();
+	m_viewport_camera->m_frustum_near = camera_functions.nearClippingPlane();
+
+	m_viewport_camera->SetAspectRatio(1.0f);
+	m_viewport_camera->SetFov(AI_RAD_TO_DEG(camera_functions.horizontalFieldOfView()));
 }
--- src/plugin/parsers/camera_parser.hpp
+++ src/plugin/parsers/camera_parser.hpp
@@ -1,14 +1,32 @@
 #pragma once
 
+// Maya API
+#include <maya/MApiNamespace.h>
+
+// C++ standard
+#include <memory>
+
+namespace wr
+{
+	class CameraNode;
+}
+
 namespace wmr
 {
 	class CameraParser
 	{
 	public:
-		CameraParser();
-		~CameraParser();
+		CameraParser() = default;
+		~CameraParser() = default;
+
+		//! Fetches a reference to the renderer
+		void Initialize();
+
+		//! Set the Wisp camera to the currently active Maya camera
+		void UpdateViewportCamera(const MString& panel_name);
 
 	private:
-
+		//! Pointer to the Wisp camera used to render the scene
+		std::shared_ptr<wr::CameraNode> m_viewport_camera;
 	};
 }
