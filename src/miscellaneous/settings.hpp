9dee1e68aff42bea85929c1c042d2736a11a8cc2
Tahar Meijs <tntmeijs@gmail.com>
2019-02-26 10:42:10 +0100

    Pool sizes
    [ADD] Variables that allow us to easily configure the pool sizes.

--- src/miscellaneous/settings.hpp
+++ src/miscellaneous/settings.hpp
@@ -9,6 +9,18 @@ namespace wmr
 	//! Holds all global settings for the plug-in
 	namespace settings
 	{
+		//! Number of textures a texture pool can hold
+		static const constexpr std::uint32_t MAX_NUMBER_OF_TEXTURES_IN_POOL = 128;
+
+		//! Size of the texture pool in MB
+		static const constexpr std::uint32_t TEXTURE_POOL_SIZE_MB = 512;
+
+		//! Maximum amount of vertex data in the model pool in MB
+		static const constexpr std::uint32_t MAX_VERTEX_DATA_SIZE_MB = 512;
+
+		//! Maximum amount of index data in the model pool in MB
+		static const constexpr std::uint32_t MAX_INDEX_DATA_SIZE_MB = 512;
+
 		//! Name of the studio / company developing this product
 		static const constexpr char* COMPANY_NAME = "Team Wisp";
 
