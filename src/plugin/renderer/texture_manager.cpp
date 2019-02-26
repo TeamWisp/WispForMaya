7b8403bc3d4d4f8fd9b40ce29fcb2362829b032d
Tahar Meijs <tntmeijs@gmail.com>
2019-02-26 10:43:32 +0100

    Create a texture pool
    [ADD] A texture pool is now created upon construction.

--- src/plugin/renderer/texture_manager.cpp
+++ src/plugin/renderer/texture_manager.cpp
@@ -1,12 +1,21 @@
 #include "texture_manager.hpp"
 
+// Wisp plug-in
+#include "plugin/viewport_renderer_override.hpp"
+#include "renderer.hpp"
+#include "settings.hpp"
+
 // Wisp rendering framework
 #include "wisp.hpp"
+#include "d3d12/d3d12_renderer.hpp"
 
 namespace wmr
 {
 	TextureManager::TextureManager()
 	{
+		// Create a texture pool using the D3D12 Wisp renderer
+		m_texture_pool.reset(dynamic_cast<const ViewportRendererOverride*>(MHWRender::MRenderer::theRenderer()->findRenderOverride(settings::VIEWPORT_OVERRIDE_NAME))->GetRenderer().GetD3D12Renderer().CreateTexturePool(settings::TEXTURE_POOL_SIZE_MB, settings::MAX_NUMBER_OF_TEXTURES_IN_POOL).get());
+
 		// The default texture needs to be loaded at all times
 		m_texture_container.push_back(m_texture_pool->Load("./resources/textures/default_texture.png", false, false));
 	}
