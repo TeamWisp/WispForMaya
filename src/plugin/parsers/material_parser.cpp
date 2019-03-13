be37f3d401bda2f8d896904297d5220dedbf82fd
Tahar Meijs <tntmeijs@gmail.com>
2019-03-13 14:27:31 +0100

    Shader data from mesh
    [ADD] Function that find the materials attached to meshes.

--- src/plugin/parsers/material_parser.cpp
+++ src/plugin/parsers/material_parser.cpp
@@ -1,9 +1,76 @@
 #include "material_parser.hpp"
 
-wmr::MaterialParser::MaterialParser()
-{
-}
+// Maya API
+#include <maya/MFnMesh.h>
+#include <maya/MObjectArray.h>
+#include <maya/MIntArray.h>
+#include <maya/MFnDependencyNode.h>
 
-wmr::MaterialParser::~MaterialParser()
+// C++ standard
+#include <vector>
+
+// https://nccastaff.bournemouth.ac.uk/jmacey/RobTheBloke/www/research/maya/mfnmesh.htm
+void wmr::MaterialParser::Parse(const MFnMesh& mesh)
 {
+	// Number of instances of this mesh
+	std::uint32_t instance_count = mesh.parentCount();
+
+	for (auto instance_index = 0; instance_index < instance_count; ++instance_index)
+	{
+		// Attach a function set to the instance
+		MFnDependencyNode mesh_fn(mesh.parent(instance_index));
+
+		// References to the shaders used on the meshes
+		MObjectArray shaders;
+
+		// Indices to the materials in the object array
+		MIntArray material_indices;
+
+		// Get all attached shaders for this instance
+		mesh.getConnectedShaders(instance_index, shaders, material_indices);
+
+		switch (shaders.length())
+		{
+			// No shaders applied to this mesh instance
+			case 0:
+				{}
+				break;
+
+			// All faces use the same material
+			case 1:
+				{
+					// Access the shader here:
+					// auto x = shaders[0]
+				}
+				break;
+
+			// Two or more materials are used
+			default:
+				{
+					// Get the number of materials by doing:
+					// auto num_of_mats = shaders.length()
+					
+					// Holds sorted face indices based on their applied material
+					std::vector<std::vector<std::uint32_t>> faces_by_material_index;
+
+					// Make sure the container has the same size as the number of shaders
+					faces_by_material_index.resize(shaders.length());
+
+					for (auto material_index = 0; material_index < material_indices.length(); ++material_index)
+					{
+						faces_by_material_index[material_indices[material_index]].push_back(material_index);
+					}
+
+					for (auto shader_index = 0; shader_index < shaders.length(); ++shader_index)
+					{
+						// Get all faces used by this material index
+						for (auto itr = faces_by_material_index[shader_index].begin(); itr != faces_by_material_index[shader_index].end(); ++itr)
+						{
+							// Use the material index here
+						}
+					}
+				}
+				break;
+		}
+	}
 }
--- src/plugin/parsers/material_parser.hpp
+++ src/plugin/parsers/material_parser.hpp
@@ -1,14 +1,35 @@
 #pragma once
 
+// Maya API
+#include <maya/MApiNamespace.h>
+
 namespace wmr
 {
+	namespace detail
+	{
+		enum class SurfaceShaderType
+		{
+			UNSUPPORTED = -1,
+
+			LAMBERT,
+			PHONG,
+			BLINN_PHONG
+		};
+	}
+
+	struct MaterialData
+	{
+	};
+
 	class MaterialParser
 	{
 	public:
-		MaterialParser();
-		~MaterialParser();
+		MaterialParser() = default;
+		~MaterialParser() = default;
+
+		void Parse(const MFnMesh& mesh);
 
 	private:
-
+		const detail::SurfaceShaderType GetShaderType();
 	};
 }
