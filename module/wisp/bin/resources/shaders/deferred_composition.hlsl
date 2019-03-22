4ac6aa4823661a7c14ca317fd451ff24a58deffa
Jens Hagen <hagen-j@outlook.com>
2019-03-22 11:24:03 +0100

    [update] shaders

--- module/wisp/bin/resources/shaders/deferred_composition.hlsl
+++ module/wisp/bin/resources/shaders/deferred_composition.hlsl
@@ -1,4 +1,5 @@
 #define LIGHTS_REGISTER register(t3)
+#define MAX_REFLECTION_LOD 3
 
 #include "fullscreen_quad.hlsl"
 #include "util.hlsl"
@@ -10,10 +11,14 @@ Texture2D gbuffer_albedo_roughness : register(t0);
 Texture2D gbuffer_normal_metallic : register(t1);
 Texture2D gbuffer_depth : register(t2);
 //Consider SRV for light buffer in register t3
-Texture2D skybox : register(t4);
-TextureCube irradiance_map : register(t5);
-RWTexture2D<float4> output : register(u0);
-SamplerState s0 : register(s0);
+TextureCube skybox : register(t4);
+TextureCube irradiance_map   : register(t5);
+TextureCube pref_env_map	 : register(t6);
+Texture2D brdf_lut			 : register(t7);
+Texture2D buffer_refl_shadow : register(t8); // xyz: reflection, a: shadow factor
+RWTexture2D<float4> output   : register(u0);
+SamplerState point_sampler   : register(s0);
+SamplerState linear_sampler  : register(s1);
 
 cbuffer CameraProperties : register(b0)
 {
@@ -21,6 +26,7 @@ cbuffer CameraProperties : register(b0)
 	float4x4 projection;
 	float4x4 inv_projection;
 	float4x4 inv_view;
+	uint is_hybrid;
 };
 
 static uint min_depth = 0xFFFFFFFF;
@@ -47,7 +53,7 @@ void main_cs(int3 dispatch_thread_id : SV_DispatchThreadID)
 	float3 pos = unpack_position(float2(uv.x, 1.f - uv.y), depth_f, inv_projection, inv_view);
 	float3 camera_pos = float3(inv_view[0][3], inv_view[1][3], inv_view[2][3]);
 	float3 V = normalize(camera_pos - pos);
-
+	
 	float3 retval;
 	
 	if(depth_f != 1.0f)
@@ -60,19 +66,39 @@ void main_cs(int3 dispatch_thread_id : SV_DispatchThreadID)
 
 		float3 flipped_N = normal;
 		flipped_N.y *= -1;
-		const float3 sampled_irradiance = irradiance_map.SampleLevel(s0, flipped_N, 0).xyz;
 
-		const float shadow_factor = 1.0f;
+		const float3 sampled_irradiance = irradiance_map.SampleLevel(linear_sampler, flipped_N, 0).xyz;
+		const float2 sampled_brdf = brdf_lut.SampleLevel(point_sampler, float2(max(dot(normal, V), 0.01f), roughness), 0).rg;
+		const float3 sampled_environment_map = pref_env_map.SampleLevel(linear_sampler, reflect(-V, normal), roughness * MAX_REFLECTION_LOD);
+
+		// Get shadow factor (0: fully shadowed, 1: no shadow)
+		float shadow_factor = lerp(
+			// Do deferred shadow (fully lit for now)
+			1.0,
+			// Shadow buffer if its hybrid rendering
+			buffer_refl_shadow[screen_coord].a,
+			// Lerp factor (0: no hybrid, 1: hybrid)
+			is_hybrid);
+
+		shadow_factor = clamp(shadow_factor, 0.0, 1.0);
 		
-		float3 skybox_reflection = skybox.SampleLevel(s0, SampleSphericalMap(reflect(-V, normal)), 0);
 
-		retval = shade_pixel(pos, V, albedo, metallic, roughness, normal, sampled_irradiance, skybox_reflection);
 
-		retval = retval * shadow_factor;
+		// Get reflection
+		float3 reflection = lerp(
+			// Sample from environment if it IS NOT hybrid rendering
+			sampled_environment_map,
+			// Reflection buffer if it IS hybrid rendering
+			buffer_refl_shadow[screen_coord].xyz,	
+			// Lerp factor (0: no hybrid, 1: hybrid)
+			is_hybrid);
+
+		// Shade pixel
+		retval = shade_pixel(pos, V, albedo, metallic, roughness, normal, sampled_irradiance, reflection, sampled_brdf, shadow_factor);
 	}
 	else
 	{	
-		retval = skybox.SampleLevel(s0, SampleSphericalMap(-V), 0);
+		retval = skybox.SampleLevel(linear_sampler, -V, 0);
 	}
 
 	//Do shading
--- module/wisp/bin/resources/shaders/lighting.hlsl
+++ module/wisp/bin/resources/shaders/lighting.hlsl
@@ -24,7 +24,7 @@ float3 shade_light(float3 pos, float3 V, float3 albedo, float3 normal, float met
 	uint tid = light.tid & 3;
 
 	//Light direction (constant with directional, position dependent with other)
-	float3 L = (lerp(light.pos - pos, light.pos - pos, tid == light_type_directional));
+	float3 L = (lerp(light.pos - pos, -light.dir, tid == light_type_directional));
 	float light_dist = length(L);
 	L /= light_dist;
 
@@ -44,7 +44,7 @@ float3 shade_light(float3 pos, float3 V, float3 albedo, float3 normal, float met
 	return lighting;
 }
 
-float3 shade_pixel(float3 pos, float3 V, float3 albedo, float metallic, float roughness, float3 normal, float3 irradiance, float3 reflection)
+float3 shade_pixel(float3 pos, float3 V, float3 albedo, float metallic, float roughness, float3 normal, float3 irradiance, float3 reflection, float2 brdf, float shadow_factor)
 {
 	float3 res = float3(0.0f, 0.0f, 0.0f);
 
@@ -57,15 +57,24 @@ float3 shade_pixel(float3 pos, float3 V, float3 albedo, float metallic, float ro
 		res += shade_light(pos, V, albedo, normal, metallic, roughness, lights[i]);
 	}
 
+
+	// Ambient Lighting using Irradiance for Diffuse
 	float3 kS = F_SchlickRoughness(max(dot(normal, V), 0.0f), metallic, albedo, roughness);
 	float3 kD = 1.0f - kS;
 	kD *= 1.0f - metallic;
 
 	float3 diffuse = irradiance * albedo;
-	float3 specular = reflection * kS;
+
+	// Image-Based Lighting using Prefiltered Environment Map and BRDF LUT for Specular
+	float3 prefiltered_color = reflection;
+	float2 sampled_brdf = brdf;
+	
+	float3 specular = prefiltered_color * (kS * sampled_brdf.x + sampled_brdf.y);
+	//float3 specular = reflection * kS;
+	
 	float3 ambient = (kD * diffuse + specular) * 1.0f; //Replace 1.0f with AO, when we have it.
 
-	return ambient + res;
+	return ambient + (res * shadow_factor);
 }
 
 float3 shade_light(float3 pos, float3 V, float3 albedo, float3 normal, float metallic, float roughness, Light light, inout uint rand_seed, uint depth)
@@ -96,34 +105,9 @@ float3 shade_light(float3 pos, float3 V, float3 albedo, float3 normal, float met
 	float3 wpos = pos + (normal * EPSILON);
 
 	// Offset shadow ray direction to get soft-shadows
-#ifdef SOFT_SHADOWS
-
-	float shadow_factor = 0.0;
-	[unroll(MAX_SHADOW_SAMPLES)]
-	for (uint i = 0; i < MAX_SHADOW_SAMPLES; ++i)
-	{
-		// Perhaps change randomness to not be purely random, but algorithm-random?
-		float3 offset = normalize(float3(nextRand(rand_seed), nextRand(rand_seed), nextRand(rand_seed))) - 0.5;
-		// Hard-coded 0.05 is to minimalize the offset a ray gets
-		// Should be determined by the area that the light is emitting from
-		offset *= 0.05;
-		float3 shadow_direction = normalize(L + offset);
-
-		bool shadow = TraceShadowRay(1, wpos, shadow_direction, t_max, depth + 1);
-
-		shadow_factor += lerp(1.0, 0.0, shadow);
-	}
-
-	shadow_factor /= float(MAX_SHADOW_SAMPLES);
+	float shadow_factor = GetShadowFactor(wpos, L, t_max, depth, rand_seed);
 
 	lighting *= shadow_factor;
-
-#else /* ifdef SOFT_SHADOWS */
-
-	bool shadow = TraceShadowRay(1, wpos, L, t_max, depth + 1);
-	lighting *= !shadow;
-
-#endif
 
 	return lighting;
 }
@@ -141,4 +125,32 @@ float3 shade_pixel(float3 pos, float3 V, float3 albedo, float metallic, float ro
 	}
 
 	return res;
+}
+
+float DoShadowAllLights(float3 wpos, uint depth, inout float rand_seed)
+{
+	uint light_count = lights[0].tid >> 2;	//Light count is stored in 30 upper-bits of first light
+
+	float res = 0;
+
+	for (uint i = 0; i < light_count; i++)
+	{
+		// Get light and light type
+		Light light = lights[i];
+		uint tid = light.tid & 3;
+
+		//Light direction (constant with directional, position dependent with other)
+		float3 L = (lerp(light.pos - wpos, -light.dir, tid == light_type_directional));
+		float light_dist = length(L);
+		L /= light_dist;
+
+		// Get maxium ray length (depending on type)
+		float t_max = lerp(light_dist, 100000, tid == light_type_directional);
+
+		// Add shadow factor to final result
+		res += GetShadowFactor(wpos, L, t_max, depth + 1, rand_seed);
+	}
+
+	// return final res
+	return res / float(light_count);
 }
