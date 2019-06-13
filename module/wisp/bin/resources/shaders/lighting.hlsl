be301a89edca6c6ad10c2154ec697b92ab330799
Meine Zeinstra <meinezeinstra@hotmail.com>
2019-06-13 11:33:18 +0200

    [FIX] All issues with the merge of the latest Wisp framework

--- module/wisp/bin/resources/shaders/lighting.hlsl
+++ module/wisp/bin/resources/shaders/lighting.hlsl
@@ -1,4 +1,12 @@
-#include "shadow_ray.hlsl"
+#ifndef __LIGHTING_HLSL__
+#define __LIGHTING_HLSL__
+
+#define CALLINGPASS_SHADOWS 0
+#define CALLINGPASS_REFLECTIONS 1
+#define CALLINGPASS_PATHTRACING 2
+#define CALLINGPASS_FULLRAYTRACING 3
+
+#include "dxr_shadow_functions.hlsl"
 
 struct Light
 {
@@ -10,6 +18,9 @@ struct Light
 
 	float3 dir;			//Direction for spot & directional
 	float ang;			//Angle for spot; in radians
+
+	float3 padding;
+	float light_size;
 };
 
 StructuredBuffer<Light> lights : LIGHTS_REGISTER;
@@ -18,7 +29,7 @@ static uint light_type_point = 0;
 static uint light_type_directional = 1;
 static uint light_type_spot = 2;
 
-float3 calc_attenuation(Light light, float3 L, float3 light_dist)
+float calc_attenuation(Light light, float3 L, float light_dist)
 {
 	uint tid = light.tid & 3;
 
@@ -84,7 +95,7 @@ float3 shade_pixel(float3 pos, float3 V, float3 albedo, float metallic, float ro
 	return ambient + res + emissive;
 }
 
-float3 shade_light(float3 pos, float3 V, float3 albedo, float3 normal, float metallic, float roughness, Light light, inout uint rand_seed, uint depth)
+float3 shade_light(float3 pos, float3 V, float3 albedo, float3 normal, float metallic, float roughness, Light light, inout uint rand_seed, uint shadow_sample_count, uint depth, uint calling_pass)
 {
 	uint tid = light.tid & 3;
 
@@ -104,15 +115,14 @@ float3 shade_light(float3 pos, float3 V, float3 albedo, float3 normal, float met
 
 	float3 wpos = pos + (normal * EPSILON);
 	
-	// Offset shadow ray direction to get soft-shadows
-	float shadow_factor = GetShadowFactor(wpos, L, t_max, depth, rand_seed);
+	float shadow_factor = GetShadowFactor(wpos, L, light.light_size, t_max, shadow_sample_count, depth, calling_pass, rand_seed);
 
 	lighting *= shadow_factor;
 
 	return lighting;
 }
 
-float3 shade_pixel(float3 pos, float3 V, float3 albedo, float metallic, float roughness, float3 emissive, float3 normal, inout uint rand_seed, uint depth)
+float3 shade_pixel(float3 pos, float3 V, float3 albedo, float metallic, float roughness, float3 emissive, float3 normal, inout uint rand_seed, uint shadow_sample_count, uint depth, uint calling_pass)
 {
 	uint light_count = lights[0].tid >> 2;	//Light count is stored in 30 upper-bits of first light
 
@@ -121,17 +131,18 @@ float3 shade_pixel(float3 pos, float3 V, float3 albedo, float metallic, float ro
 	[unroll]
 	for (uint i = 0; i < light_count; i++)
 	{
-		res += shade_light(pos, V, albedo, normal, metallic, roughness, lights[i], rand_seed, depth);
+		res += shade_light(pos, V, albedo, normal, metallic, roughness, lights[i], rand_seed, shadow_sample_count, depth, calling_pass);
 	}
 
 	return res + emissive;
 }
 
-float4 DoShadowAllLights(float3 wpos, float3 V, float3 normal, float metallic, float roughness, float3 albedo, uint depth, inout float rand_seed)
+float4 DoShadowAllLights(float3 wpos, float3 V, float3 normal, float metallic, float roughness, float3 albedo, uint shadow_sample_count, uint depth, uint calling_pass, inout float rand_seed)
 {
 	uint light_count = lights[0].tid >> 2;	//Light count is stored in 30 upper-bits of first light
 
 	float4 res = float4(0.0, 0.0, 0.0, 0.0);
+	uint sampled_lights = 0;
 
 	for (uint i = 0; i < light_count; i++)
 	{
@@ -144,7 +155,19 @@ float4 DoShadowAllLights(float3 wpos, float3 V, float3 normal, float metallic, f
 		float light_dist = length(L);
 		L /= light_dist;
 
+		float dir_dot = dot(L, normal);
+
+		if (dir_dot < 0.0f)
+		{
+			continue;
+		}
+
 		float attenuation = calc_attenuation(light, L, light_dist);
+
+		if (light_dist > light.rad && tid != light_type_directional)
+		{
+			continue;
+		}
 
 		float3 radiance = attenuation * light.col;
 
@@ -154,15 +177,19 @@ float4 DoShadowAllLights(float3 wpos, float3 V, float3 normal, float metallic, f
 		float t_max = lerp(light_dist, 100000, tid == light_type_directional);
 
 		// Add shadow factor to final result
-		float shadow = GetShadowFactor(wpos, L, t_max, depth, rand_seed);
+		float shadow = GetShadowFactor(wpos, L, light.light_size, t_max, shadow_sample_count, depth, calling_pass, rand_seed);
 
 		res.w += shadow;
 
 		res.rgb += lighting * shadow;
+
+		sampled_lights++;
 	}
 
 	// return final res
-	res.w = res.w / float(light_count);
+	res.w = res.w / float(sampled_lights);
 
 	return res;
 }
+
+#endif //__LIGHTING_HLSL__
