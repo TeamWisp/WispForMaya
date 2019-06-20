95e1a0e969f650cf81e3577edda8fe9e77a0eefc
Meine Zeinstra <meinezeinstra@hotmail.com>
2019-06-20 11:43:48 +0200

    [ADD] Render settings class

---
+++ src/miscellaneous/render_settings.cpp
@@ -0,0 +1,14 @@
+#include "render_settings.hpp"
+
+// Maya includes
+#include <maya/MGlobal.h>
+#include <maya/MString.h>
+
+// STD includes
+
+
+namespace wmr
+{
+
+}
+
---
+++ src/miscellaneous/render_settings.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// STD includes
+#include <string>
+
+//! Generic plug-in namespace (Wisp Maya Renderer)
+namespace wmr
+{
+	//! RenderSettings
+	/*! This class gets you specific graphics settings from Wisp and is able to set them as well. */
+	class RenderSettings final
+	{
+	public:
+
+
+	};
+}
